1a22bcdce63ec8e41e2d00a454fae177c7019af0
vladdd1792 <107995468+vladdd1792@users.noreply.github.com>
2022-08-28 16:00:52 +0200

    Add files via upload

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -4,10 +4,8 @@
     closeMenuBtn: document.querySelector('[data-menu-close]'),
     menu: document.querySelector('[data-menu]'),
   };
-
   refs.openMenuBtn.addEventListener('click', toggleModal);
   refs.closeMenuBtn.addEventListener('click', toggleModal);
-
   function toggleModal() {
     refs.menu.classList.toggle('is-open');
   }
